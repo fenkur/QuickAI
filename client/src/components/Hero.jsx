25908c9c6b720428d493233e7d55afaa7a680b78
Fenky <fenkywah15@gmail.com>
2025-09-29 17:53:17 -0500

    added: laptop progress

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom'
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Hero = () => {
     const navigate = useNavigate();
@@ -21,6 +22,10 @@ const Hero = () => {
             transition cursor-pointer'>Start creating now!</button>
             <button className='bg-white px-10 py-3 rounded-lg border border-gray-300 hover:scale-102
             active:scale-95 transition cursor-pointer'>Watch demo</button>
+        </div>
+
+        <div className='flex items-center gap-4 mt-8 mx-auto text-gray-600'>
+          <img src={assets.user_group} alt='' className='h-8'/> Trusted by 10k+ people
         </div>
     </div>
   )
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
 import Hero from '../components/Hero'
+import AiTools from '../components/AITools'
 
 const Home = () => {
   return (
     <>
       <NavBar/>
       <Hero />
+      <AiTools />
     </>
   )
 }
