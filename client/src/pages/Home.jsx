0ba570de47f6798a5317986d73bab3683557ca54
Fenky Wah <fenkwah15@gmail.com>
2025-09-29 13:07:30 -0500

    feat: Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import NavBar from '../components/NavBar'
+import Hero from '../components/Hero'
 
 const Home = () => {
   return (
-    <div>
-        <h1>Home</h1>
-    </div>
+    <>
+      <NavBar/>
+      <Hero />
+    </>
   )
 }
 
