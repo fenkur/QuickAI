b958dc83e3aecb85e2e942b3ed9c32661fcd6123
Fenky <fenkywah15@gmail.com>
2025-10-01 14:28:12 -0500

    added: sidebar + dashboard page

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-// import assets from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
 import { ArrowRight } from 'lucide-react'
 import { assets } from '../assets/assets'
@@ -11,7 +10,7 @@ const NavBar = () => {
     const { openSignIn } = useClerk();
 
   return (
-    <div className='fixed z-10 w-full bg-white flex justify-between items-center py-3 px-4 sm:px-20 xl:px-32'>
+    <div className='fixed z-10 w-full flex justify-between items-center py-3 px-4 sm:px-20 xl:px-32'>
         <img src={assets.logo} alt="logo" className='w-32 sm:w-44 cursor-pointer' onClick={()=>navigate('/')}/>
         
         {
