463c6c4d803f93ea6db03be5583a5abb98bb46b8
Fenky <fenkywah15@gmail.com>
2025-10-02 16:40:22 -0500

    added: finished pages

--- client/src/pages/WriteArticle.jsx
+++ client/src/pages/WriteArticle.jsx
@@ -26,7 +26,7 @@ const WriteArticle = () => {
         </div>
         <p className='mt-6 text-sm font-medium'>Article Topic</p>
         <input onChange={(e) => setInput(e.target.value)} value={input} type='text' className='w-full p-2 px-3 mt-2 outline-none text-sm
-        rounded-md border border-gray-300' placeholder='The future of artificial intelligence is...' required></input>
+        rounded-md border border-gray-300' placeholder='The future of artificial intelligence is...' required/>
 
         <p className='mt-4 text-sm font-medium'>Article Length</p>
 
@@ -40,15 +40,28 @@ const WriteArticle = () => {
         </div>
         <br/>
         <button className='w-full flex justify-center items-center gap-2
-        bg-gradient-to-r from-[#227BFF] to-[#65ADFF] text-white px-4 py-2 mt-6
+        bg-gradient-to-r from-[#65ADFF] to-[#227BFF] text-white px-4 py-2 mt-6
         text-sm rounded-lg cursor-pointer'>
           <Edit className='w-5'/>
           Generate Article
         </button>
       </form>
       {/* Right Column */}
-      <div>
+      <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border
+      border-gray-200 min-h-96 max-h-[600px]'>
+        <div className='flex items-center gap-3'>
+          <Edit className='w-5 h-5 text-[#4A7AFF]'/>
+          <h1 className='text-xl font-semibold'>Generated Article</h1>
+        </div>
 
+        <div className='flex-1 flex justify-center items-center'>
+          <div className='text-sm flex flex-col items-center gap-5
+          text-gray-400'>
+            <Edit className='w-9 h-9'/>
+            <p>Enter a topic and click "Generate Article" to get started</p>
+
+          </div>
+        </div>
       </div>
     </div>
   )
